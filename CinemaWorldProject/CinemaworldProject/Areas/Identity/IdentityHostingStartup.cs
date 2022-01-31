6141c05eab8e2803aa160dcb3441005180144da6
David-Popov <79060381+David-Popov@users.noreply.github.com>
2022-01-31 20:51:42 +0200

    Some design changes

---
+++ CinemaWorldProject/CinemaworldProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CinemaworldProject.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CinemaworldProject.Areas.Identity.IdentityHostingStartup))]
+namespace CinemaworldProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
